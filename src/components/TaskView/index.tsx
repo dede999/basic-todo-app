a26febc4eb8ca4abf497062b73059998a3b07ee1
André Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2020-05-29 19:08:08 -0300

    Improved layout

--- src/components/TaskView/index.tsx
+++ src/components/TaskView/index.tsx
@@ -1,3 +1,4 @@
+import './index.sass'
 import React from 'react'
 import ToDoContext, { ITask } from '../../providers/ToDoContext';
 import { Input, Checkbox, Button } from '@material-ui/core';
@@ -8,7 +9,8 @@ interface Props {
 
 interface State {
   the_task: ITask,
-  original_task: string
+  original_task: string,
+  edition_mode: boolean
 }
 
 export default class TaskView extends React.Component<Props, State> {
@@ -17,7 +19,8 @@ export default class TaskView extends React.Component<Props, State> {
       title: "",
       is_complete: false
     },
-    original_task: ""
+    original_task: "",
+    edition_mode: false
   }
   
   componentDidMount() {
@@ -31,29 +34,62 @@ export default class TaskView extends React.Component<Props, State> {
     })
   }
 
+  complete_class = (): string => {
+    const { is_complete } = this.state.the_task
+    return is_complete ? 'complete' : 'incomplete'
+  }
+
   render() {
     const { title, is_complete } = this.state.the_task
-    const { original_task, the_task } = this.state
+    const { original_task, the_task, edition_mode } = this.state
 
     return(
       <ToDoContext.Consumer>
         {({edit_task, remove_task}) => (
-          <div className="task-list-item">
-            <Checkbox 
-              checked={is_complete} 
-              onChange={(e: any) => this.setState({
-                the_task: { title: title, is_complete: !is_complete }
-              })} />
-            <Input
-              placeholder={title}
-              onChange={(e: any) => this.setState({
-                the_task: { title: e.target.value, is_complete: is_complete }
-              })} />
+          <div className={`task-list-item ${this.complete_class()}`}>
+            <div className="main">
+              { edition_mode 
+                ? <>
+                    <Checkbox 
+                      checked={is_complete}
+                      color="primary"
+                      onChange={(e: any) => this.setState({
+                        the_task: { title: title, is_complete: !is_complete }
+                      })} 
+                    />
+                    <Input
+                      placeholder={title}
+                      onChange={(e: any) => this.setState({
+                        the_task: { title: e.target.value, is_complete: is_complete }
+                      })} 
+                    />
+                  </>
+                : 
+                <>
+                  <span className="title"> { title } </span>
+                </>
+              }
+            </div>
             <div className="actions">
-              <Button onClick={() => edit_task(the_task, original_task)}>
+            { edition_mode
+              ? 
+              <Button variant="outlined" 
+                onClick={() => {
+                  edit_task(the_task, original_task)
+                  this.setState({ edition_mode: false })
+              }}>
                 Save
               </Button>
-              <Button onClick={() => remove_task(title)}>
+              :
+              <Button variant="outlined" 
+                onClick={() => this.setState({
+                  edition_mode: true
+              })}>
+                Edit
+              </Button>              
+            }
+              <Button variant="outlined" 
+                onClick={() => remove_task(title)}>
                 Delete
               </Button>
             </div>
